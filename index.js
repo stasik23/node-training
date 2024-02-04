18a5372c1b78fbaddad6dc06489ef07977083b51
stasik23 <118220114+stasik23@users.noreply.github.com>
2024-02-04 18:27:53 +0200

    xxx

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const PORT = 3000;
+import express from "express";
+import 'dotenv/config'
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 5000
 
-app.get('/',(req,res)=>{
-    res.send('Welcome PussyDestroyer228')
-})
+app.get('/', (req, res) => {
+  res.send('Welcome to my server!');
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on port:${PORT}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
